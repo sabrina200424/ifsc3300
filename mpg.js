2cb51cae96642d134b297beddcbc38626d1bc554
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-02-05 01:14:38 +0000

    Added in mpg homework

---
+++ mpg.js
@@ -0,0 +1,20 @@
+let again = "y";
+
+do {
+    const miles = parseFloat(prompt("Enter miles driven"));
+    const gallons = parseFloat(prompt("Enter gallons of gas used"));
+
+    if (miles > 0 && gallons > 0) {
+        const mpg = parseFloat(miles/gallons);
+
+        const html = `<p>${miles} miles on ${gallons} 
+                      gallons = ${mpg.toFixed(3)} MPG</p>`;
+        document.write(html);
+    } 
+    else {
+        alert("One or both entries are invalid");
+    }
+ 
+    again = prompt("Repeat entries? (y/n)", "y");
+}
+while (again == "y");
