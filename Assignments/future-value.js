599849271ee17afca611b9ed7f12c720ee25c9f9
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-02-26 03:43:56 +0000

    Added html modifications to future value assignment

--- Assignments/future-value.js
+++ Assignments/future-value.js
@@ -21,13 +21,27 @@ return futureValue.toFixed(2)
 
 //---------------------------------------------------------------------------------
 
-const processEntries = () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const processEntries = () => {
+    
+        $("#join_list").addEventListener("click", evt => {
+            // get values user entered in textboxes
+            const investment = parseFloat($("#investment_number").value);
+            const interestRate = parseFloat($("#interest_Rate").value);
+            const years = parseFloat($("#years_number").value);
+            
+            let errorMessage = "";
 
-
-    if (isNaN(investment) || investment <= 0 || investment > 100000) {
+    if (isNaN(investment) && investment <= 0 && investment > 100000) {
         errorMessage = 'Investment must be a number greater than zero and less than or equal to 100,000.';
     }
-
-
-
-}
+    if (isNaN(interestRate) && interestRate <= 0 && interestRate > 15) {
+        errorMessage = 'Investment must be a number greater than zero and less than or equal to 15.';
+    }
+    if (isNaN(years) && years <= 0 && years > 50) {
+    errorMessage = 'Years must be a number greater than zero and less than or equal to 50.';
+    }
+    if (errorMessage === '') {
+        const futureValue = calculateFV(investment, interestRate, years);
+    }
+})}});
