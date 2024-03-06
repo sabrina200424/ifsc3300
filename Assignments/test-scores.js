52a1b6e13e4ce5ecc86c13eb13a6ab31a1d8b752
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-03-06 02:18:24 +0000

    Added comments in an older document

--- Assignments/test-scores.js
+++ Assignments/test-scores.js
@@ -13,10 +13,10 @@ total += score2;
 const score3 = parseInt(prompt("Enter test score"));
 total += score3;
 
-//calculate the average
+//calculate the average scores from total
 const average = parseInt(total/3);
 
-// display the scores
+// display the scores on the website
 const html = "<p>Score 1 = " + score1 + "</p>" +
     "<p>Score 2 = " + score2 + "</p>" +
     "<p>Score 3 = " + score3 + "</p>" +
