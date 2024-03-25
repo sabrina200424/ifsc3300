1f71a43a738cf053385e489b251a634587c3b5a0
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-03-25 15:47:54 +0000

    Added in easing class example

--- Exercise Testing/easing.js
+++ Exercise Testing/easing.js
@@ -1,7 +1,23 @@
-$("#image_list a").hover(
-    evt => $(evt.currentTarget).stop(true).animate(
-        { top: 15 }, "fast"),
-    evt => $(evt.currentTarget).stop(true).animate(
-        { top: 0 }, "fast")
-);
+"use strict";
+$(document).ready( () => {
+    
+    $("#faqs h1").click( evt => {
+        $(evt.currentTarget)
+            .animate({ fontSize: "650%", opacity: 1, left: "+=275" }, 
+                2000, "easeInExpo" )  
+            .animate({ fontSize: "175%", left: "-=275" }, 
+                1000, "easeOutExpo" );
+    });
 
+    $("#faqs h2").click( evt => {
+        const target = evt.currentTarget;
+        $(target).toggleClass("minus");
+        if ($(target).attr("class") == "minus") {
+            $(target).next().slideDown(1000, "easeOutBounce");
+        }
+        else {
+            $(target).next().slideUp(1000, "easeInBounce");
+        }
+    });
+    
+});
