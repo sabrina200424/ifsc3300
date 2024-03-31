e749ba5f0b614faedfa069a5b4118d7bad756d42
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-03-31 16:53:58 +0000

    Added in exercise 9-1

---
+++ Assignments/animation.js
@@ -0,0 +1,32 @@
+$(document).ready( () => {
+	
+	// runs when an h2 heading is clicked
+    $("#faqs h2").click( evt => {
+		const h2 = evt.currentTarget;
+
+		$(h2).toggleClass("minus");
+
+		if ($(h2).attr("class") != "minus") {
+			$(target).next().slideDown(
+				1000, "easeOutBounce");	
+	   	}
+	   	else {
+	        $(h2).next().show();
+			$(target).next().slideUp(
+				1000, "easeInBounce");	
+		   }
+		   
+		evt.preventDefault();
+    }); // end click
+    
+    // runs when the page is ready
+    $("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 1000 )  
+		         .animate( { fontSize: "175%", left: "-=200" }, 1000 );
+		    
+	// runs when the top-level heading is clicked
+	$("#faqs h1").click( () => {
+		$("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 2000 )  
+			         .animate( { fontSize: "175%", left: "-=200" }, 1000 );
+	}); // end click
+    
+}); // end ready
