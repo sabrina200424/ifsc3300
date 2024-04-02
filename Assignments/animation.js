a36629cb6ceb24bb177425e3e12a29c8165151b8
sabrina200424 <125308110+sabrina200424@users.noreply.github.com>
2024-04-02 03:52:08 +0000

    Finished exercise 9-1

--- Assignments/animation.js
+++ Assignments/animation.js
@@ -1,10 +1,12 @@
 "use strict";
+//add in strict derative if clicking isn't working
 $(document).ready( () => {
     
     $("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 1000 )  
 		         .animate( { fontSize: "175%", left: 0 }, 1000 );
 		    
 	// runs when the top-level heading is clicked
+    // is set to target h1 link
 	$("#faqs h1").click( () => {
 		$("#faqs h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 2000 )  
 			         .animate( { fontSize: "175%", left: 0 }, 1000 );
@@ -16,6 +18,8 @@ $(document).ready( () => {
         if ($(target).attr("class") == "minus") {
             $(target).next().slideDown(1000, "easeOutBounce");
         }
+        //slide no. 32 from chapter 9 content 
+		//put under target function
         else {
             $(target).next().slideUp(1000, "easeInBounce");
         }
